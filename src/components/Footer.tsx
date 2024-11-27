00ebe98b247b23692fa9b627eadee5bfa71f6fb2
gregoriodelucca <gregoriodelucca@gmail.com>
2024-11-27 20:17:40 -0300

    mudando a parte do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Meu Portfólio. Todos os direitos reservados.</p>
+        <p> Desenvolvido gregorio de lucca. &copy; {new Date().getFullYear()}</p>
       </div>
     </footer>
   );

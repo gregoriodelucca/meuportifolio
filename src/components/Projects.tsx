0cbc22ede47632e16efd3695161ba982144fbf10
gregoriodelucca <gregoriodelucca@gmail.com>
2024-11-27 23:29:10 -0300

    autalizando mais um projeto

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -22,10 +22,10 @@ const projects: Project[] = [
     image: "https://aasoft.com.br/images/logo.png", // Coloque o caminho correto da imagem
   },
   {
-    title: "Github",
-    description: "github",
-    link: "https://github.com/gregoriodelucca",
-    image: "https://camo.githubusercontent.com/2366b34bb903c09617990fb5fff4622f3e941349e846ddb7e73df872a9d21233/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f3733303730332f73637265656e73686f74732f363538313234332f6176656e746f2e676966", // Coloque o caminho correto da imagem
+    title: "igreja batista nacional da mooca",
+    description: "Bem-vindo à Igreja Batista da Mooca!",
+    link: "https://igrejabatistamooca.vercel.app",
+    image: "https://ugc.production.linktr.ee/Mb7mrVZRIaQWfSPz8jEn_hu34oaud2K6K9TdH?io=true&size=avatar-v3_0", // Coloque o caminho correto da imagem
   },
   // Adicione mais projetos conforme necessário
 ];
